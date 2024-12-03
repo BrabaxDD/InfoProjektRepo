3d353282b9f685c39716456226703a01f81e94cf
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-12-03 15:56:02 +0100

    Chat GPT beispiel ANimation eingefügt

--- game/views.py
+++ game/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World")
+
+
+def game(request):
+    context = {"content": "Willkommen im Djungel"}
+    return render(request, "index.html", context)
