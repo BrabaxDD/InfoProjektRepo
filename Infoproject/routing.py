1bbe88984515a29b312fbcf2906b20a87fea6893
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-12-05 17:06:01 +0100

    bug fixed

--- Infoproject/routing.py
+++ Infoproject/routing.py
@@ -1,4 +1,4 @@
 from channels.routing import URLRouter
 from django.urls import re_path
 import game.routing
-router = URLRouter([re_path("game/",game.routing.app)])
+router = URLRouter([re_path("game",game.routing.app)])
