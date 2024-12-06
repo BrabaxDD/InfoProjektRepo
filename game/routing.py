25e27ea37d4fb726a875f32a6a17d5d32a529b79
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-12-06 16:51:23 +0100

    halb gefixter bug

--- game/routing.py
+++ game/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from channels.routing import URLRouter
 from .consumers import gameServer, gamePlayerSocket
 
-app = URLRouter([#re_path(   "server", gameServer.gameServer.as_asgi()),
-    re_path("", gamePlayerSocket.gamePlayerSocketConsumer.as_asgi())])
+app = URLRouter([re_path("", gameServer.gameServer.as_asgi()),
+                 # re_path("", gamePlayerSocket.gamePlayerSocketConsumer.as_asgi())
+                 ])
