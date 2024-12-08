3ad6a353c45e49050baaee81c3f1ef7ad16e9de3
Klavierfloh <grund.georg@web.de>
2024-12-08 15:00:04 +0100

    Button with Text

--- game/static/GUI_elements/Button.js
+++ game/static/GUI_elements/Button.js
@@ -1,7 +1,7 @@
 import GameObject from "../GameObject.js"
 
 export default class ButtonGameObject extends GameObject {
-    constructor(posx, posy, widthButton, heightButton, eventString, scene) {
+    constructor(posx, posy, widthButton, heightButton, eventString, scene, text) {
         super(scene)
         this.posx = posx
         this.posy = posy
@@ -12,6 +12,12 @@ export default class ButtonGameObject extends GameObject {
         this.ctx = this.scene.canvas.getContext("2d")
         this.scene.eventBus.registerListner("click_on_canvas", this)
         this.is_hovered = false
+        this.text = text
+        this.textSize = 60
+        this.textColor = "black"
+        text.ButtonPrimaryColor = "green"
+        
+
     }
     process() {
         if (this.scene.mousex >  this.posx && this.scene.mousex < this.posx + this.widhtButton && 
@@ -26,11 +32,20 @@ export default class ButtonGameObject extends GameObject {
 
     }
     render() {
-        this.ctx.fillStyle = "blue";
+        this.ctx.fillStyle = this.ButtonPrimaryColor;
         if (this.is_hovered == true){
-            this.ctx.fillStyle = "red"
+            this.ctx.fillStyle = this.ButtonScondaryColor
         }
         this.ctx.fillRect(this.posx,this.posy,this.widhtButton,this.heightButton)
+
+        this.ctx.fillStyle = this.textColor;
+        this.ctx.strokeStyle = this.textColor;
+        this.ctx.font = this.textSize;
+        this.ctx.textAlign = 'center';
+        this.ctx.textBaseline = 'middle';
+        
+        this.ctx.fillText(this.text, this.posx+(this.widhtButton / 2), this.posy+(this.heightButton / 2));
+        //this.ctx.strokeText(text, this.posx+(this.widhtButton / 2), this.posy+(this.heightButton / 2));
     }
 
     buttonPresed(){
@@ -41,6 +56,22 @@ export default class ButtonGameObject extends GameObject {
         if (eventString == "click_on_canvas" && this.is_hovered == true) {
             this.buttonPresed()
         }
+    }
+
+    setTextSize(size){
+        this.text
+    }
+
+    setTextColor(color){
+        this.textColor = color
+    }
+
+    setButtonColorPrimary(color){
+        this.ButtonPrimaryColor = color
+    }
+
+    setButtonColorSecondary(color){
+        this.ButtonScondaryColor
     }
 
 }
--- game/static/GameSceneFactory.js
+++ game/static/GameSceneFactory.js
@@ -14,7 +14,7 @@ export default class GameSceneFactory{
             case "mainMenu":
             case 0:
                 let player = new Player(100,100,20,20, 'blue', 5, this.keys,scene)
-                let button = new ButtonGameObject(200,200,50,50,"testButtonPress",scene)
+                let button = new ButtonGameObject(200,200,50,50,"testButtonPress",scene,"Test Button")
                 scene.addObject(player)
                 scene.addObject(button)
                 break;
