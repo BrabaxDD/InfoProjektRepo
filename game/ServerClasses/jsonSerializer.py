5b545726ef5baf5679450f1cb3cecc43d9e0091f
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-12-20 18:28:21 +0100

    bug fix

--- game/ServerClasses/jsonSerializer.py
+++ game/ServerClasses/jsonSerializer.py
@@ -4,6 +4,6 @@ from game.ServerClasses import ItemsStack
 
 def asDict(obj):
     if isinstance(obj, Inventory.Inventory):
-        return {"items": obj.items}
+        return obj.__dict__
     elif isinstance(obj, ItemsStack.ItemStack):
-        return {"size": obj.size, "itemID": obj.itemID, "tags": obj.tags}
+        return obj.__dict__
