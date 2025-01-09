317e04cbcff5f515acc407025f1fbaa3c6178f6b
Klavierfloh <grund.georg@web.de>
2025-01-09 20:05:48 +0100

    aiahbfae

--- game/static/Scene.js
+++ game/static/Scene.js
@@ -145,7 +145,7 @@ export default class Scene {
             console.log(eventObject)
             const length = this.gameObjects.length
             for (let i = 0; i <= length; i++) {
-                if (this.gameObjects[i].ID === this.eventObject.ID) {
+                if (this.gameObjects[i].ID === eventObject.ID) {
                     console.log("OBJECT TO DELETE: ID: ", eventObject.ID, " Type: ", eventObject.type)
                     console.log("OBJECT THAT IS DELETED: Typ: " + this.gameObjects[i].constructor.name)
                     this.removeObject(this.gameObjects[i])
